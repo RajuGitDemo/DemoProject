d643cb50b4aaf1c9be0a18d18247f4f65de857a5
RajuVikram <vikramraju9551@gmail.com>
2020-09-08 17:51:14 +0530

    UpladFileCode_Completed

--- src/app/shared/file-upload.service.ts
+++ src/app/shared/file-upload.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileUploadService {
+  apiUrl: 'https://api.github.com/users/hadley/orgs';
+  myApiUri: "https://localhost:44365/Employee/upload"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  upload(formData) {
+    //return this.http.post('${this.apiUrl}', formData,{reportProgress:true,observe:'events'}).pipe(catchError(this.handleError))
+   return this.http.post('https://localhost:44365/Employee/upload', formData, {reportProgress: true, observe: 'events'}).pipe(catchError(this.handleError))
+
+  }
+
+  
+
+  handleError(error: HttpErrorResponse) {
+    console.log("Error------------lalalalalalalala");
+    return throwError(error);
+  }
 }
--- src/app/uploads/uploads.component.ts
+++ src/app/uploads/uploads.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import {FileUploadService} from 'src/app/shared/file-upload.service'
+import { HttpClient, HttpEventType } from '@angular/common/http';
+import { FileUploadService } from 'src/app/shared/file-upload.service'
 
 @Component({
   selector: 'app-uploads',
@@ -9,21 +9,51 @@ import {FileUploadService} from 'src/app/shared/file-upload.service'
   styleUrls: ['./uploads.component.sass']
 })
 export class UploadsComponent implements OnInit {
-  uploadForm:FormGroup;
-  constructor(private fb:FormBuilder, private http:HttpClient, private fileUploadService: FileUploadService) { }
+  uploadForm: FormGroup;
+  public progress: number;
+  public message: string;
+  @Output() public onUploadFinished = new EventEmitter();
+  constructor(private fb: FormBuilder, private http: HttpClient, private fileUploadService: FileUploadService) { }
 
   ngOnInit(): void {
-    this.uploadForm=this.fb.group({
-      name:[''],
-      emprofile:['']     
-    });
+    // this.uploadForm = this.fb.group({
+    //   name: [''],
+    //   emprofile: ['']
+    // });
   }
-  onSelectedFile(event){
-    console.log(event.target.files);
-  }
-  onSubmmit(){
-    const formData=new FormData();
+  // onSelectedFile(event) {
+  //   console.log(event.target.files);
+  //   if (event.target.files.length > 0) {
+  //     const uploadFile = event.target.files[0];
+  //     this.uploadForm.get('emprofile').setValue(uploadFile);
+
+  //   }
+  // }
+  // onSubmmit() {
+  //   const formData = new FormData();
+  //   formData.append('name', this.uploadForm.get('name').value);
+  //   formData.append('emprofile', this.uploadForm.get('emprofile').value);
+  //   this.fileUploadService.upload(formData).subscribe(events => this.fileresult = events,error=>this.myError=error);
+  // }
+
+  public uploadFile = (files) => {
+    debugger;
+    if (files.length === 0) {
+      return;
+    }
+    console.log("call service")
+    let fileToUpload = <File>files[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    this.fileUploadService.upload(formData)
+      .subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress)
+          this.progress = Math.round(100 * event.loaded / event.total);
+        else if (event.type === HttpEventType.Response) {
+          this.message = 'Upload success.';
+          this.onUploadFinished.emit(event.body);
+        }
+      });
   }
 
 }
- 
